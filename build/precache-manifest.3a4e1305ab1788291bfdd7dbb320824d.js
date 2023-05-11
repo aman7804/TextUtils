a88141aacf9cdbd7539b26e121025182b3ac9947
aman7804 <bhateriyaaman@gmail.com>
2023-05-11 11:02:38 -0700

    added files

---
+++ build/precache-manifest.3a4e1305ab1788291bfdd7dbb320824d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb81e50aecfa377400b9",
+    "url": "/static/css/main.b043922c.chunk.css"
+  },
+  {
+    "revision": "eb81e50aecfa377400b9",
+    "url": "/static/js/main.eb81e50a.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "846fe33ba27a2bef411b",
+    "url": "/static/js/2.846fe33b.chunk.js"
+  },
+  {
+    "revision": "b117a03f136dce80bebe",
+    "url": "/static/js/runtime~main.b117a03f.js"
+  },
+  {
+    "revision": "e3e7f62753c7b4407434935d756c1108",
+    "url": "/index.html"
+  }
+];
