a4b97463d6d0d7c6790ac41076f9a1c005b71444
aman <bhateriyaaman@gmail.com>
2023-05-12 10:15:14 -0700

    Updates

---
+++ precache-manifest.abbd1d45b2724c92c3ff3483f972e7ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "13743822e7b9f1810546",
+    "url": "/TextUtils/static/css/main.f1e19485.chunk.css"
+  },
+  {
+    "revision": "13743822e7b9f1810546",
+    "url": "/TextUtils/static/js/main.13743822.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "96d93f1fc62c6d632e28dfe3d2096431",
+    "url": "/TextUtils/index.html"
+  }
+];
